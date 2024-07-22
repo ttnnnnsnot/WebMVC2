81d012cf65c8cf7dc3ee2536c16f8cb728b326fd
ttnnnnsnot <59921083+ttnnnnsnot@users.noreply.github.com>
2024-07-22 15:01:47 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebMVC2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
